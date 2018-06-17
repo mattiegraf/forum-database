b9aeb5a6b11c1422337721ac30d920bcac4d7d19
madigraf <grafmadison@gmail.com>
2018-06-16 17:52:48 -0700

    added deletion options for comments and threads

--- db-app/src/Data.js
+++ db-app/src/Data.js
@@ -13,18 +13,18 @@ var Data = {
                     comments: [
                         {
                             id: 1,
-                            author:"",
-                            body: ""
+                            author:"xxslayeriscoolxx",
+                            body: "lol man why you on the cat forum then?"
                         },
                         {
                             id: 2,
-                            author:"",
-                            body: ""
+                            author:"doglover66",
+                            body: "well i mean it is cat related, i guess i just wanna know why people like em"
                         },
                         {
                             id: 3,
-                            author:"",
-                            body: ""
+                            author:"xxslayeriscoolxx",
+                            body: "lol then why didn't u ask that?  i like them cuz theyre independent but also loving"
                         }
                     ]
                 },
@@ -33,14 +33,16 @@ var Data = {
                     id: 23,
                     author: "catman22",
                     title: "i hate people who hate cats!!",
-                    body: "they must be psychos to hate them! purrfect in every way!!!"
+                    body: "they must be psychos to hate them! purrfect in every way!!!",
+                    comments: []
                 },
 
                 {
                     id: 22,
                     author: "xxslayeriscoolxx",
                     title: "here is a picture of my cat!",
-                    body: " ^ ^\n(o o)\n  |w|\n"
+                    body: " ^ ^\n(o o)\n  |w|\n",
+                    comments: []
                 }
             ]
         },
@@ -52,21 +54,35 @@ var Data = {
                     id: 33,
                     author: "doglover66",
                     title: "lets invade the cat board",
-                    body: "lol im so bored, lets go make some cat lovers mad!!!"
+                    body: "lol im so bored, lets go make some cat lovers mad!!!",
+                    comments: [
+                        {
+                            id: 1,
+                            author:"maxmax96",
+                            body: "dude leave them alone, no need to be annoying"
+                        },
+                        {
+                            id: 2,
+                            author:"doglover66",
+                            body: "wtf im not annoying"
+                        }
+                    ]
                 },
 
                 {
                     id: 11,
                     author: "maxmax96",
                     title: "my dog died :(",
-                    body: "i miss u spot, u will always be my best friend, rip"
+                    body: "i miss u spot, u will always be my best friend, rip",
+                    comments: []
                 },
 
                 {
                     id: 10,
                     author: "doglover66",
                     title: "i dressed up my dog like batman",
-                    body: "he hates it. lol i wish this site supported images"
+                    body: "he hates it. lol i wish this site supported images",
+                    comments: []
                 }
             ]
         },
@@ -78,28 +94,32 @@ var Data = {
                     id: 33,
                     author: "ubc-throwaway22",
                     title: "i hate bikers",
-                    body: "ugh they are literally devils on earth, one almost KILLED me today"
+                    body: "ugh they are literally devils on earth, one almost KILLED me today",
+                    comments: []
                 },
 
                 {
                     id: 11,
                     author: "Nike",
                     title: "down with the mods!",
-                    body: "they have gone too far, it is time we rise up"
+                    body: "they have gone too far, it is time we rise up",
+                    comments: []
                 },
 
                 {
                     id: 10,
                     author: "kimchimallet",
                     title: "Average to get into CS? HELP",
-                    body: "guys i really need to get in cs :("
+                    body: "guys i really need to get in cs :(",
+                    comments: []
                 },
 
                 {
                     id: 12,
                     author: "ubcel",
                     title: "tinder help",
-                    body: "i only swipe right for 4 years and no matches. guys be real am i gonna be alone forever.... i am in engineering and male"
+                    body: "i only swipe right for 4 years and no matches. guys be real am i gonna be alone forever.... i am in engineering and male",
+                    comments: []
                 }
             ]
         }
